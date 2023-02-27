4fb5a3f3ac46c8225ebbc6ee76a59fab38aaff76
Laptop <anurag6713@gmail.com>
2023-02-27 15:56:48 +0530

    Code updated

--- modules/message/middlewares/getMessages.middleware.ts
+++ modules/message/middlewares/getMessages.middleware.ts
@@ -1,7 +1,10 @@
 import {ObjectId} from 'mongodb';
-import {MAX_MESSAGES_FETCH_LIMIT} from '@config';
 
+import {MAX_MESSAGES_FETCH_LIMIT} from '@config';
+import {permissions} from '@constants/permissions';
+import channelServices from '@modules/channel/services';
 import channelMemberServices from '@modules/channelMember/services';
+import teamRAPServices from '@modules/teamRAPServices/services';
 import utils from '@utils';
 
 import type {Middleware} from '@customTypes';
@@ -33,6 +36,34 @@ const getMessages = (): Middleware => {
             return {
                 status: 401,
                 message: 'UNAUTHORIZED',
+            };
+        }
+
+        // 3. Get the teamId from the channelId
+        const channelData = await channelServices.getBy('_id', channelId, {
+            teamId: 1,
+        });
+        if (!channelData) {
+            return {
+                status: 400,
+                message: 'INVALID_INPUT',
+            };
+        }
+
+        const {teamId} = channelData;
+
+        // 4. Check if user has permission to view messages
+        const hasFetchPermission = await teamRAPServices.hasPermission(
+            teamId,
+            channelId,
+            userId,
+            permission: permissions.MESSAGES.VIEW,
+        );
+
+        if (!hasFetchPermission) {
+            return {
+                status: 401,
+                message: 'NO_PERMISSION',
             };
         }
 
